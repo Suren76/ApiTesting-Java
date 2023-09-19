6917f5b293eccd33e4e3b61825300cbbd05c7640
Suren <parsyansuren@gmail.com>
2023-09-19 21:55:09 +0400

    remove all cached files

--- src/main/java/exceptions/UserExists.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UserExists extends RuntimeException{
-    public UserExists(String message) {
-        super("Yo bro, there are the user with the same email, check error message "+message);
-    }
-}
--- src/test/java/BaseTest.java
+++
@@ -1,9 +0,0 @@
-import org.testng.annotations.BeforeClass;
-import services.ReqresIn;
-
-public class BaseTest {
-    @BeforeClass
-    void setup() {
-        ReqresIn.setup();
-    }
-}
